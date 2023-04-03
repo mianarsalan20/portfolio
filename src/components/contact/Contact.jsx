d445a25d2fa4de1804fae6d13c5b22851756dbc8
Muhammad Arsalan <mianarsalan20@gmail.com>
2023-04-03 21:13:13 +0500

    Fetch present year for copyrights

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,53 +1,42 @@
-import React from 'react'
-import './contact.css'
-import Phone from '../../assets/phone.svg';
-import Email from '../../assets/email.svg';
-import Homee from '../../assets/home.svg';
+import React from "react";
+import "./contact.css";
+import Phone from "../../assets/phone.svg";
+import Email from "../../assets/email.svg";
+import Homee from "../../assets/home.svg";
 
 const Contact = () => {
+  const year = new Date().getFullYear();
   return (
     <section className="contact section" id="contact">
-    <h2 className="section__title">Get in touch</h2>
-    <span className="section__subtitle">Contact me</span>
+      <h2 className="section__title">Get in touch</h2>
+      <span className="section__subtitle">Contact me</span>
 
-    <div className="contact__container container grid">
-
+      <div className="contact__container container grid">
         <div className="contact__box">
           <div>
-            <img src={Phone} className="contact__icon"/>
+            <img src={Phone} className="contact__icon" alt="Phone-No" />
             <h3 className="contact__title"> +92 303 5540078 </h3>
           </div>
-
-
         </div>
 
         <div className="contact__box">
           <div>
-            <img src={Email} className="contact__icon"/>
+            <img src={Email} className="contact__icon" alt="Email" />
             <h3 className="contact__title">mianarsalan20@gmail.com</h3>
           </div>
-
-
         </div>
 
         <div className="contact__box">
           <div>
-            <img src={Homee} className="contact__icon"/>
+            <img src={Homee} className="contact__icon" alt="Address" />
             <h3 className="contact__title">Islamabad, Pakistan </h3>
           </div>
-
-
-          
-
-
         </div>
+      </div>
 
-      
-    </div>
+      <span className="footer">© All Copyright Reserved {year}</span>
+    </section>
+  );
+};
 
-    <span className="footer">© All Copyright Reserved 2023</span>
-</section>
-  )
-}
-
-export default Contact
+export default Contact;
